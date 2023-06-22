755b8eb3173eda8164a710741aa630ba61dead70
Princy Rasolonjatovo <princy.m.rasolonjatovo@gmail.com>
2023-06-22 15:13:37 +0300

    basics

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -10,109 +10,78 @@ import { INITIAL_EVENTS, createEventId } from './event-utils';
 import { AddEventComponent } from './add-event/add-event.component';
 import { EventService } from '../services/event.service';
 import { Subscription } from 'rxjs';
+import { IEvent } from 'models/event';
 
 @Component({
-  selector: 'app-calendar',
-  templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.scss']
+	selector: 'app-calendar',
+	templateUrl: './calendar.component.html',
+	styleUrls: ['./calendar.component.scss']
 })
-export class CalendarComponent implements OnInit , OnDestroy{
-  calendarVisible = true;
+export class CalendarComponent implements OnInit, OnDestroy {
+	calendarVisible = true;
 
-  currentEvents: EventApi[] = [];
+	currentEvents: EventApi[] = [];
 
-  events : any[] = [];
-  eventSubscription : Subscription;
+	calendarOptions: CalendarOptions;
 
-  constructor(
-    private changeDetector: ChangeDetectorRef,
-    public dialog: MatDialog,
-    private eventService : EventService
-  ) {
-    this.eventSubscription = this.eventService.eventsSubject.subscribe(
-      (events : any[]) => {
-        this.events = events;
-      }
-    );
-    this.eventService.emitEvents();
-   }
-  ngOnDestroy(): void {
-    this.eventSubscription.unsubscribe();
-  }
+	events: IEvent[] = [];
+	eventSubscription: Subscription;
 
-  ngOnInit(): void {
-    //this.eventService.saveEvents();
-    this.eventService.getEvents();
-  }
+	constructor(
+		public dialog: MatDialog,
+		private eventService: EventService
+	) {
+		this.eventSubscription = this.eventService.eventsSubject.subscribe(
+			(events: IEvent[]) => {
+				this.events = events;
+			}
+		);
 
-  calendarOptions: CalendarOptions = {
-    plugins: [
-      interactionPlugin,
-      dayGridPlugin,
-      timeGridPlugin,
-      listPlugin,
-    ],
-    headerToolbar: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-    },
-    initialView: 'dayGridMonth',
-    initialEvents: INITIAL_EVENTS, // alternatively, use the `events` setting to fetch from a feed
-    weekends: true,
-    editable: true,
-    selectable: true,
-    selectMirror: true,
-    dayMaxEvents: true,
-    select: this.handleDateSelect.bind(this),
-    eventClick: this.handleEventClick.bind(this),
-    eventsSet: this.handleEvents.bind(this)
-  };
+		this.eventService.emitEvents();
 
-  handleCalendarToggle() {
-    this.calendarVisible = !this.calendarVisible;
-  }
 
-  handleWeekendsToggle() {
-    const { calendarOptions } = this;
-    calendarOptions.weekends = !calendarOptions.weekends;
-  }
+		this.calendarOptions = {
+			plugins: [
+				interactionPlugin,
+				dayGridPlugin,
+				timeGridPlugin,
+				listPlugin,
+			],
+			headerToolbar: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+			},
+			initialView: 'dayGridMonth',
+			// initialEvents: INITIAL_EVENTS, // alternatively, use the `events` setting to fetch from a feed
+			initialEvents: [{ title: 'Meeting', start: new Date() }],
+			weekends: true,
+			editable: true,
+			selectable: true,
+			selectMirror: true,
+			dayMaxEvents: true,
+			eventMouseEnter : this.onHoverEnter.bind(this),
+			eventMouseLeave : this.onHoverLeave.bind(this)
+			// select: this.handleDateSelect.bind(this),
+			// eventClick: this.handleEventClick.bind(this),
+			// eventsSet: this.handleEvents.bind(this)
+		};
+	}
+	ngOnDestroy(): void {
+		this.eventSubscription.unsubscribe();
+	}
 
-  handleDateSelect(selectInfo: DateSelectArg) {
-    const title = prompt('Please enter a new title for your event');
-    const calendarApi = selectInfo.view.calendar;
+	ngOnInit(): void {
+		this.eventService.getEvents();
+	}
 
-    calendarApi.unselect(); // clear date selection
+	onHoverEnter(...args: any[])
+	{
+		console.log(args)
+	}
 
-    if (title) {
-      calendarApi.addEvent({
-        id: createEventId(),
-        title,
-        start: selectInfo.startStr,
-        end: selectInfo.endStr,
-        allDay: selectInfo.allDay
-      });
-    }
+	onHoverLeave(...args: any[])
+	{
 
-  }
-
-  handleEventClick(clickInfo: EventClickArg) {
-    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-      clickInfo.event.remove();
-    }
-  }
-
-  handleEvents(events: EventApi[]) {
-    this.currentEvents = events;
-    this.changeDetector.detectChanges();
-  }
-
-  openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
-    this.dialog.open(AddEventComponent, {
-      width: '100%',
-      height: '20rem',
-      enterAnimationDuration,
-      exitAnimationDuration,
-    });
-  }
+	}
 }
